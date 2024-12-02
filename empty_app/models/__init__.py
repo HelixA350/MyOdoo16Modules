7655e976936d736f3028c5e7870f309a9389afc0
HelixA350 <aleksandrfaleev3@gmail.com>
2024-12-02 15:27:18 +0300

    2_November_formating_not_finished

--- empty_app/models/__init__.py
+++ empty_app/models/__init__.py
@@ -1,3 +1,5 @@
 from . import report
 from . import feedback
 from . import format
+from . import exlWrapper
+from . import xl_work_class
--- empty_app/models/format.py
+++ empty_app/models/format.py
@@ -4,6 +4,9 @@ import base64
 import openpyxl
 import io
 import logging
+from .exlWrapper import ExcelWrapper
+from .xl_work_class import Xl_work
+import openpyxl as oxl
 
 _logger = logging.getLogger(__name__) 
 
@@ -15,11 +18,21 @@ class Format(models.Model):
 
     file_f = fields.Binary(required=True, string='Форматировать')
 
+    def format_file(self, path_file:str, path_done:str) -> str:
+        ew = ExcelWrapper(['Вложения', 'Последний раз обновлено', 'Статус', 'Наименование сервисного центра'], ['ПЭ: дата время', 'ПЭ: Комментарий', 'ПЭ: наработка м/ч'], path_file)
+        ew.format()
+        wb = oxl.load_workbook(filename=path_file)
+        for sheet in wb.sheetnames[2:]:
+            ew.formatTitles(wb[sheet], True)
+            ew.formattingCells(wb[sheet])
+        wb.save(path_done)
+        wb.close()
+
     def send(self):
         file_data = base64.b64decode(self.file_f)  # Декодируем переданный файл
         xlsx_data = io.BytesIO(file_data)  # Конвертируем в поток
-
         
+        self.format_file(xlsx_data, xlsx_data)
 
         _logger.info('success')
         return None
