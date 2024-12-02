2dbb1daea8cfb932e0adc8fd517037aa09707098
HelixA350 <aleksandrfaleev3@gmail.com>
2024-12-02 15:01:50 +0300

    logging_btn_action

--- empty_app/models/format.py
+++ empty_app/models/format.py
@@ -1,4 +1,11 @@
 from odoo import fields, models
+from odoo import models, fields, api
+import base64
+import openpyxl
+import io
+import logging
+
+_logger = logging.getLogger(__name__) 
 
 class Format(models.Model):
     _name = "format"
@@ -6,7 +13,13 @@ class Format(models.Model):
 
     name = fields.Char(required=True, string="Имя")
 
-    file_f = fields.Binary(required=False, string='Форматировать')
+    file_f = fields.Binary(required=True, string='Форматировать')
 
     def send(self):
-        pass
+        file_data = base64.b64decode(self.file_f)  # Декодируем переданный файл
+        xlsx_data = io.BytesIO(file_data)  # Конвертируем в поток
+
+        
+
+        _logger.info('success')
+        return None
